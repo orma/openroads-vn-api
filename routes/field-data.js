2b2e6f702993664fdbace6d0a1e87afab852f183
maxgrossman <m.grossman1813@gmail.com>
2017-09-22 16:51:16 -0400

    put transformation into own function

--- routes/field-data.js
+++ routes/field-data.js
@@ -6,89 +6,71 @@ var fc = require('@turf/helpers').featureCollection;
 var map = require('lodash').map;
 var groupBy = require('lodash').groupBy;
 
+/**
+ * @func groupGeometriesById
+ * Given an array of objects that represent field data geometries,
+ * returns an array of objects that, for each road ids, include road geometries grouped by source.
+ * @param geoms {array} array of field data goemetry representing data
+ * @return {array} array of field data geometries grouped by road id
+ */
+function groupGeometriesById(geoms) {
+  // first group geometries by road id.
+  // this top level grouping allows for the 'group by source' to be applied to each road id's data
+  geoms = groupBy(geoms, 'road_id');
+  // for each road id group:
+  //   1. take geometries and group them by source
+  //   2. take each source group and generate a feature collection
+  var groupedGeoms = map(geoms, (roadId ,k) => {
+    // group road id geometries by their source
+    let sourceGroup = groupBy(roadId, 'source');
+    // for each source group, make a feature collection
+    sourceGroup = map(sourceGroup, (sourceGeometry, i) => {
+      // transform each sourceGeometry into a feature collection reporesentation of it
+      sourceGeometry = map(sourceGeometry, (sourceObj, j) => {
+        // make properties object that includes the sourceObj's
+        // source name as well as the roadId
+        let props = {properties: {source: sourceObj.source, vProMMsId: k}};
+        // parse geometry string to a json
+        let geom = JSON.parse(sourceObj.geometry);
+        // return props and geom as a single object
+        return Object.assign(geom, props);
+      });
+      // aftet being passsed through the map function, sourceGeometry is an array of geojson features,
+      // so passing it through turf's feature collection function turns it into a feature collection
+      return fc(sourceGeometry);
+    });
+    // finally, take source group, currently an array of sourceGeometry feature collections,
+    // and put it in a object that will use the road id as its key and the feature collections list as its 
+    // property
+    const roadObject = {};
+    roadObject[k] = sourceGroup;
+    // return that object.
+    return roadObject;
+  });
+  return groupedGeoms;
+}
+
 module.exports = [
   {
-     /**
-   * @api {get} /field/{id}/geometries Field Data geometries with particular vProMMs id
-   * @apiGroup Fieldp
+  /**
+   * @api {get} /field/{ids}/geometries Group Road field data by source
+   * @apiGroup Field
+   * @apiName FieldGeometries
+   * @apiDescription Returns list of each road id's field data, grouped by source (either RoadLabPro or RouteShoot)
    * @apiVersion 0.1.0
-   *
-   * @apiParam {Number} vProMMSid road id
-   * 
-   * @apiSuccess {Array} FeatureCollections feature collections of geometries unique to source (either RoadLabPro or RouteShoot)
-   *
-   * @apiExample {curl} Example Usage:
-   *    curl http://localhost:4000/field/101010/geometries
-   *
-   * @apiSuccessExample {json} Success-Response:
-   * [
-   *   {"101010":
-   *     [
-   *       {
-   *         "type":"FeatureCollection",
-   *         "features":[
-   *           {"type":"Point",
-   *            "coordinates":[10.809003,54.097834],
-   *            "properties":{"source":"RoadLabPro"}},
-   *           {"type":"Point",
-   *            "coordinates":[10.810003,54.097834],
-   *            "properties":{"source":"RoadLabPro"}}
-   *         ]
-   *       },
-   *       {
-   *         "type":"FeatureCollection",
-   *         "features":[
-   *           {"type":"Point",
-   *            "coordinates":[10.809003,54.097834],
-   *            "properties":{"source":"RouteShoot"}},
-   *           {"type":"Point",
-   *            "coordinates":[10.814003,54.097834],
-   *            "properties":{"source":"RouteShoot"}}
-   *          ]
-   *        }
-   *      ]
-   *    },
-   *    {"024LC00004":[{"type":"FeatureCollection","features":[{"type":"Point","coordinates":[10.94003,54.7834],"properties":{"source":"RouteShoot"}}]}]}]
    */
     method: 'GET',
     path: '/field/{ids}/geometries',
     handler: function (req, res) {
       // get the vpromms id supplied in request parameters
       const ids = req.params.ids.split(',');
-      // select records with matching id from field_data_geometries
+      // select roads with
       knex('field_data_geometries')
       .whereIn('road_id', ids)
       .select('type as source', 'road_id', knex.raw(`ST_AsGeoJSON(geom) as geometry`))
       .then(geoms => {
-        // group results by id
-        geoms = groupBy(geoms, 'road_id');
-        // map each id group into feature collections
-        // where each feature collection includes geometries separated by source
-        geoms = map(geoms, (id ,k) => {
-          // group reponses by source
-          id = groupBy(id, 'source');
-          // for each source group, make a feature collection
-          id = map(id, (s, i) => {
-            // map each raw response (here s) to a feature collection
-            s = map(s, (o, j) => {
-              // make properties object from query's source prop.
-              let props = {properties: {source: o.source}};
-              // parse ST_ASGeoJSON text as geometry.
-              let geom = JSON.parse(o.geometry);
-              // return props and geom as a single object
-              return Object.assign(geom, props);
-            });
-            return fc(s);
-          });
-          // put id into an object that has its actual id as its key
-          const idGeom = {};
-          idGeom[k] = id;
-          // return that object.
-          return idGeom;
-        });
-        // serve the response
-        console.log(JSON.stringify(geoms));
-        res(geoms);
+        let groupedGeometries = groupGeometriesById(geoms);
+        res(groupedGeometries);
       })
       .catch(e => {
         // deal with any errors
